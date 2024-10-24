f95c591e3307d7598f118c37cd42b3d46173ec19
Aakash Rambhad <146167024+AakashRam70@users.noreply.github.com>
2024-10-24 19:18:33 +0530

    first commit

--- constructor.php
+++ constructor.php
@@ -1,3 +1,4 @@
+<!-- PHP supports a special type of method called constructor for initializing an object when it is created. This is known as automatic initialization of objects. -->
 <?php
 class Student{
     public $roll;
